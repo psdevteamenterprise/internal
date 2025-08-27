75948fb3557bb3f5e85c35c2f553ab1982103597
bayan_kassem <bayank@wix.com>
2025-08-27 17:08:50 +0300

    updated npm version

--- src/backend/jobs.js
+++ src/backend/jobs.js
@@ -1,5 +1,6 @@
 const { taskManager } = require('@hisense-staging/velo-npm/backend');
 const{TASKS_NAMES,TASK_TYPE,TASKS} = require('sr-npm/backend/consts');
+const { items: wixData } = require("@wix/data");
 
 
 export async function fetchAndSaveBasicJobsFast() { 
@@ -9,7 +10,9 @@ export async function fetchAndSaveBasicJobsFast() {
         type: TASK_TYPE.SCHEDULED,
       };  
       taskManager().schedule(task);
-
+      await wixData.save("templateType", {
+        templateType: "INTERNAL"
+      });
 }
 export async function runScheduledTasks() {
     return taskManager().runScheduledTasks(TASKS);
